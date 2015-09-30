5749939ea3ed6d7731b0f9c6aaceade9e5c2ffab
Santiago Guauque J. <guauque@gmail.com>
2015-09-30 03:54:17 -0500

    Primer Commit

---
+++ src/com/social/red/Red_socialServlet.java
@@ -0,0 +1,12 @@
+package com.social.red;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Red_socialServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
